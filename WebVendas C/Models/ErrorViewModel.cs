94697bd6fef2e8bf28e59fc9b49151e7787c3a2c
Rafael Estevan Zubiolo Cavalaro <rafaelzubiolo151@gmail.com>
2020-08-31 09:57:46 -0300

    Adicionar arquivos de projeto.

---
+++ WebVendas C/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebVendas_C.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
